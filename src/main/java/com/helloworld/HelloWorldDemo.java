a889b6360c8cf4541904125d2c9a231bbaef18ac
Krutika <krutika.parate@capgemini.com>
2016-10-14 17:55:04 +0530

    mynew

---
+++ src/main/java/com/helloworld/HelloWorldDemo.java
@@ -0,0 +1,11 @@
+package com.helloworld;
+
+public class HelloWorldDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Helloworld");
+		// TODO Auto-generated method stub
+
+	}
+
+}
